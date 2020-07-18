3e758d6607424726829580fe6ecedc2ef2f26d39
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-18 18:18:22 +0530

    few mistakes corrected.

--- jokes/src/main/java/com/example/jokes/controller/JokesController.java
+++ jokes/src/main/java/com/example/jokes/controller/JokesController.java
@@ -1,19 +1,28 @@
 package com.example.jokes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.jokes.service.JokeServiceImpl;
+import com.example.jokes.service.JokeService;
 
 @Controller
 public class JokesController {
 	
+	private JokeService jokeService;
 	
-	@RequestMapping("")
+	@Autowired
+	public JokesController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+
+
+
+	@RequestMapping({"/",""})
 	public String viewJoke(Model model) {
 		
-		//model.addAttribute(model.);
+		model.addAttribute("joke",jokeService.getJoke());
 		
 		return "joke/chucknorris";
 	}
