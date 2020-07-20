b3f5cdc85bbbcb4500283353a04ab88722115a7c
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-20 19:08:01 +0530

    Bean Configuration explained

---
+++ jokes/src/main/java/com/example/jokes/config/ChuckConfig.java
@@ -0,0 +1,18 @@
+package com.example.jokes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Configuration
+public class ChuckConfig {
+	
+	@Bean
+	public ChuckNorrisQuotes ChuckNorrisQuotes() {
+		return new ChuckNorrisQuotes();
+	}
+
+	
+	
+}
--- jokes/src/main/java/com/example/jokes/service/JokeServiceImpl.java
+++ jokes/src/main/java/com/example/jokes/service/JokeServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.jokes.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
@@ -10,15 +9,25 @@ public class JokeServiceImpl implements JokeService{
 
 	private final ChuckNorrisQuotes myClass;
 	
-	@Autowired
-	public JokeServiceImpl() {
-		this.myClass = new ChuckNorrisQuotes();
+	
+	// Normal Implementation
+	/*
+	 * @Autowired public JokeServiceImpl() { this.myClass = new ChuckNorrisQuotes();
+	 * }
+	 */
+	
+	
+	// When we are using Bean Configuration to handle to class instance creation.
+
+	public JokeServiceImpl(ChuckNorrisQuotes myClass) {
+		this.myClass = myClass;
 	}
 
 	@Override
 	public String getJoke() {
 		return myClass.getRandomQuote();
 	}
+
 
 	
 }
